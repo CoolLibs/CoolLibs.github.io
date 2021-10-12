f369bb6d8c389b9dc6170041e6839a7a4abb6d41
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-12 16:23:10 +0200

    Test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
             },
             {to: '/blog', label: 'Blog', position: 'left'},
             {
-              href: 'https://github.com/docusaurus',
+              href: 'https://github.com/',
               label: 'GitHub',
               position: 'right',
             },
