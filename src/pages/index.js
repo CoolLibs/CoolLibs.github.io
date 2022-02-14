d959da4550d9d90a9c3c7fd84ac48ca0147fa1e7
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-14 16:07:53 +0100

    [index] Fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from "react"
+import clsx from "clsx"
+import Layout from "@theme/Layout"
+import Link from "@docusaurus/Link"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import styles from "./index.module.css"
+import HomepageFeatures from "../components/HomepageFeatures"
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline} <br/>
-        Designed for computer graphics software</p>
+        <p className="hero__subtitle">
+          {siteConfig.tagline} <br />
+          Designed for computer graphics software
+        </p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/tutorials">
+          <Link className="button button--secondary button--lg" to="/tutorials">
             Let's get started 🚀
           </Link>
         </div>
       </div>
     </header>
-  );
+  )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       title=""
-      description="An open-source C++ framework, designed for computer graphics software.">
+      description="An open-source C++ framework, designed for computer graphics software."
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
     </Layout>
-  );
+  )
 }
