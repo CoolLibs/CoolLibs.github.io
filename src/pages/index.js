83ebacf1b3f05731a1da2c0e5902715fbb24ac5d
Jules Fouchy <jules.fouchy@ntymail.com>
2022-05-18 00:31:26 +0200

    Add some words on the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,6 +36,43 @@ export default function Home() {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <div className={styles.grey}>
+          {/* <b> */}
+          <div className={styles.centered}>
+            🚀 Our main goal is to build <i>Cool</i>, a modern framework for
+            computer graphics software.
+            <br />
+            🛠️ In doing so we also develop many small libraries, independent of
+            the big framework.
+            <br />
+            🎨 With this framework we build{" "}
+            <a href="/lab">
+              a generative-art software, <i>CoolLab</i>
+            </a>
+            .
+            <br />
+            📖 We also document our process as much as possible and{" "}
+            <a href="/blog">share our learnings on our website</a>
+            .
+            <br />
+            🤗 We support other open-source projects and contribute back to all
+            the projects we rely on, either through pull requests or
+            sponsorships.
+            <br />
+            👩‍🏫 We take on interns each year because we are happy to teach young
+            developers and welcome new contributors.
+            <br />
+            <br />
+            The philosophy of our libraries is to be:
+            <ul>
+              <li> 🎁 Open-source </li>
+              <li> 🚀 Cross-platform </li>
+              <li> 😎 Easy to integrate into any project through CMake </li>
+              <li> 🛠️ Easy to build from source</li>
+            </ul>
+          </div>
+          {/* </b> */}
+        </div>
       </main>
     </Layout>
   )
